a429582421a1af90ce169910595de89ff20d3f16
Eetana <64721985+Eetana@users.noreply.github.com>
2023-02-09 14:36:16 +1100

    Added something to get price of specific stock

--- fetch.py
+++ fetch.py
@@ -1,4 +1,12 @@
-import pyasx.data.companies
+import pyasx.data.companies as pdc
+import pyasx.data.securities as pds
 
-results = pyasx.data.companies.get_listed_companies()
+results = pdc.get_listed_companies()
 print(len(results)) # Somewhere around 2k companies
+print(results[1])     # What is available is limitd, and doesn't include stock
+
+# Found the way to get price of the specific stock and the relevant metadata, including curr price (last_price)
+p = pds.get_security_info(results[0]["ticker"]) 
+
+print(p)
+print(p["last_price"])  # In $AUD
